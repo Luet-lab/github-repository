1dbe29205e6ecaf5e0518ac6a856fdb809b6be43
Ettore Di Giacinto <mudler@sabayon.org>
2021-10-14 22:20:22 +0200

    Use ghcr by default, enable also pull_request builds

--- .github/build.sh
+++ .github/build.sh
@@ -2,8 +2,16 @@
 
 # CONFIGURE: Tweak to suit your needs
 # You might want to remove $PULL_REPOSITORY if you don't need to pull for any image
+if [ "$PUSH_CACHE" == "true" ]; then
 sudo -E luet build \
         --only-target-package \
         --pull-repository $PULL_REPOSITORY \
         --pull --push --image-repository $FINAL_REPO \
         --from-repositories --no-spinner --live-output --tree packages "$1"
+else
+sudo -E luet build \
+        --only-target-package \
+        --pull-repository $PULL_REPOSITORY \
+        --pull --image-repository $FINAL_REPO \
+        --from-repositories --no-spinner --live-output --tree packages "$1"
+fi
